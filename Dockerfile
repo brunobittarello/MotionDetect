f1b14c699dbb41451cbf0c5c6f15f6313370fe9a
Bruno Bittarello <brunobitta@hotmail.com>
2024-05-20 14:09:27 -0300

    Using class and functions

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM python:3.9.19
+FROM python:3.9.19-slim
 
 RUN apt update
 RUN apt install -y ffmpeg
@@ -8,6 +8,8 @@ RUN apt install -y python3-opencv
 RUN python3 -m pip install imutils
 RUN python3 -m pip install numpy
 RUN python3 -m pip install opencv-python
+
+RUN ln -sf /bin/bash /bin/sh
 
 COPY MotionDetect/ App/
 WORKDIR /App
--- MotionDetect/motion_detect.py
+++ MotionDetect/motion_detect.py
@@ -9,143 +9,162 @@ import cv2
 import numpy as np
 import time
 
-
 # motion detection is comparision between previous_frame && current_frame
 
-FRAMES_TO_PERSIST = 10 # Updates the previous frame in every 10th frame from the loop.
-MIN_SIZE_FOR_MOVEMENT = 400 # 200 - higher is the number lesser is motion detection sensitivity. (window size)
-MOVEMENT_DETECTED_PERSISTENCE = 100 # no. of frame count down before saving the video.
+class MotionDetect:
+    FRAMES_TO_PERSIST = 10 # Updates the previous frame in every 10th frame from the loop.
+    MIN_SIZE_FOR_MOVEMENT = 400 # 200 - higher is the number lesser is motion detection sensitivity. (window size)
+    MOVEMENT_DETECTED_PERSISTENCE = 100 # no. of frame count down before saving the video.
 
+    cap = None
+    output = ""
+    movement_persistent_counter = 0
 
-
-source = "Volumes/imggen/sample.mp4" # webcam| rtsp_url | video file as input
-cap = cv2.VideoCapture(source) # Then start the webcam
-if not cap.isOpened():
-    print("Failed to open")
-    exit()
-
-# Init frame variables
-total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
-curr_frame_num = 0
-curr_frame_num_mod = 0
-first_frame = None
-next_frame = None
-
-# Init display font and timeout counters
-font = cv2.FONT_HERSHEY_SIMPLEX
-delay_counter = 0
-movement_persistent_counter = 0
-
-fourcc = cv2.VideoWriter_fourcc(*'MP4V')
-# fourcc = cv2.VideoWriter_fourcc(*'XVID') # .avi
-out = None
-
-while True:
-    transient_movement_flag = False    
-    if curr_frame_num_mod == 0:
-        print(f"reading {curr_frame_num} of {total_frames}")
-    
-    ret, frame = cap.read()
-    text = "Unoccupied"
-
-
-    if curr_frame_num == total_frames:
-        break
-
-    if not ret:
-        print("CAPTURE ERROR")
-        break
-
-    # original_frame = frame.copy()
-
-    frame = imutils.resize(frame, width = 750)
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-    gray = cv2.GaussianBlur(gray, (21, 21), 0)
-
-    if first_frame is None: first_frame = gray    
-
-    delay_counter += 1
-
-    if delay_counter > FRAMES_TO_PERSIST:
-        delay_counter = 0
-        first_frame = next_frame
-
+    def __init__(self):
+        # Init frame variables
+        self.cap = None
+        self.total_frames = 0
+        self.curr_frame_num = 0
+        self.curr_frame_num_mod = 0
         
-    next_frame = gray
+        self.movement_persistent_counter = 0        
+        self.out = None
 
-    frame_delta = cv2.absdiff(first_frame, next_frame)
-    thresh = cv2.threshold(frame_delta, 25, 255, cv2.THRESH_BINARY)[1]
+        self.fourcc = cv2.VideoWriter_fourcc(*'MP4V')
+        # fourcc = cv2.VideoWriter_fourcc(*'XVID') # .avi
+        # self.font = cv2.FONT_HERSHEY_SIMPLEX # for display only
 
-    thresh = cv2.dilate(thresh, None, iterations = 2)
-    cnts, _ = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    def apply(self, source, output):
+        self.cap = cv2.VideoCapture(source) # Then start the webcam
+        if not self.cap.isOpened():
+            print("Failed to open")
+            return
+        
+        self.output = output
+        self.total_frames = int(self.cap.get(cv2.CAP_PROP_FRAME_COUNT))
+        self.process()
 
-    for c in cnts:
-        (x, y, w, h) = cv2.boundingRect(c)
-        if cv2.contourArea(c) > MIN_SIZE_FOR_MOVEMENT:
-            if y < 18 and x < 190:
-                continue
-            # print(f"{x} {y} {w} {h} = {cv2.contourArea(c)}")
-            transient_movement_flag = True
-            # cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+    def process(self):
+        first_frame = None
+        next_frame = None
+        delay_counter = 0
 
-    if transient_movement_flag == True:
-        movement_persistent_flag = True
-        movement_persistent_counter = MOVEMENT_DETECTED_PERSISTENCE
- 
-    if movement_persistent_counter > 0:
-        # text = "Movement Detected " + str(movement_persistent_counter)
-        movement_persistent_counter -= 1
-    # else:
-        # text = "No Movement Detected"
+        while True: 
+            if self.curr_frame_num_mod == 0:
+                print(f"reading {self.curr_frame_num} of {self.total_frames}")
+            
+            ret, frame = self.cap.read()
 
-    # print("puttext")
-    # cv2.putText(frame, str(text), (10,35), font, 0.75, (255,255,255), 2, cv2.LINE_AA)
-    # print("framedelta")
-    # frame_delta = cv2.cvtColor(frame_delta, cv2.COLOR_GRAY2BGR)
-    # # cv2.imshow("frame", np.hstack((frame_delta, frame)))
-    # print("umshwo")
-    # cv2.imshow("frame", frame)
-    # print("umshwo2")
+            if self.curr_frame_num == self.total_frames:
+                break
 
+            if not ret:
+                print("CAPTURE ERROR")
+                break
 
-    # record video -----------
-    if movement_persistent_counter>0 and not out: # for the very first frame
-        height, width, _ = frame.shape
-        out = cv2.VideoWriter(f'Volumes/imggen/{int(time.time())}_video.mp4',fourcc, 30.0 ,(width, height))
-        out.write(frame)
-        print("write file")
+            frame = imutils.resize(frame, width = 750)
+            gray = self.prepare_frame(frame)
 
-    if movement_persistent_counter>0 and out:
-        out.write(frame)
+            if first_frame is None: first_frame = gray    
+
+            delay_counter += 1
+
+            if delay_counter > self.FRAMES_TO_PERSIST:
+                delay_counter = 0
+                first_frame = next_frame
+
+            next_frame = gray
+
+            if self.compare_frames(first_frame, next_frame) == True:
+                self.movement_persistent_counter = self.MOVEMENT_DETECTED_PERSISTENCE
+            elif self.movement_persistent_counter > 0:
+                self.movement_persistent_counter -= 1
+
+            # self.display(frame)
+            self.record(frame)
+            # self.saveSeparated()
+            # self.stopKey()
+            self.nextFrame()
+
+        self.close()
+
+    def prepare_frame(self, frame):
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        gray = cv2.GaussianBlur(gray, (21, 21), 0)
+        return gray
+
+    def nextFrame(self):
+        self.curr_frame_num += 1
+        if self.curr_frame_num_mod == 99:
+            self.curr_frame_num_mod = 0
+        else:
+            self.curr_frame_num_mod = 1
+
+    def compare_frames(self, frame1, frame2):
+        frame_delta = cv2.absdiff(frame1, frame2)
+        thresh = cv2.threshold(frame_delta, 25, 255, cv2.THRESH_BINARY)[1]
+
+        thresh = cv2.dilate(thresh, None, iterations = 2)
+        cnts, _ = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+        for c in cnts:
+            (x, y, w, h) = cv2.boundingRect(c)
+            if cv2.contourArea(c) > self.MIN_SIZE_FOR_MOVEMENT:
+                if y < 18 and x < 190:
+                    continue
+                # print(f"{x} {y} {w} {h} = {cv2.contourArea(c)}")
+                # cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+                return True
+        return False
+
+    def display(frame):
+        text = "Movement Detected " + str(self.movement_persistent_counter) if self.movement_persistent_counter > 0 else "No Movement Detected"
+        cv2.putText(frame, str(text), (10,35), self.font, 0.75, (255,255,255), 2, cv2.LINE_AA)
+        # frame_delta = cv2.cvtColor(frame_delta, cv2.COLOR_GRAY2BGR)
+        # cv2.imshow("frame", np.hstack((frame_delta, frame)))
+        cv2.imshow("frame", frame)
+
+    def record(self, frame):
+        if self.movement_persistent_counter == 0:
+            return
+
         print("write frame")
+        if self.out:
+            self.out.write(frame)
+            return
 
-    # Save each clip in separate video file
-    # if movement_persistent_counter == 0 and out:
-    #     out.release()
-    #     out = None
-    # ----------------------
-    
+        if not self.out: # for the very first frame
+            height, width, _ = frame.shape
+            self.out = cv2.VideoWriter(self.output, self.fourcc, 30.0 ,(width, height))
+            self.out.write(frame)
+        
 
-    # ch = cv2.waitKey(1)
-    # if ch & 0xFF == ord('q'):
-    #     if out is not None: # saving & exiting on press of q
-    #         out.release()
-    #         out = None
-    #     break
-    
-    curr_frame_num = curr_frame_num + 1
-    if curr_frame_num_mod == 99:
-        curr_frame_num_mod = 0
-    else:
-        curr_frame_num_mod = curr_frame_num_mod + 1
+    def saveSeparated(self):
+        if self.movement_persistent_counter == 0 and out:
+            self.out.release()
+            self.out = None
 
-# Cleanup when closed
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
+    def stopKey(self):
+        ch = cv2.waitKey(1)
+        if ch & 0xFF == ord('q'):
+            if self.out is not None: # saving & exiting on press of q
+                self.out.release()
+                self.out = None
+            return True
+        return False
 
-if out != None:
-    out.release()
+    def close(self):
+        # Cleanup when closed
+        # cv2.waitKey(0)
+        # cv2.destroyAllWindows()
 
-cap.release()
-print("END")
+        if self.out != None:
+            self.out.release()
+
+        self.cap.release()
+        print("END")
+
+md = MotionDetect()
+source = "Volumes/imggen/sample.mp4" # webcam| rtsp_url | video file as input'
+output = f'Volumes/imggen/{int(time.time())}_video.mp4'
+md.apply(source, output)
